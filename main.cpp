482c73a1bfaa67adbd151e4bba3b8b41ea4fec71
joye2003 <leoscchou@gmail.com>
2022-04-18 20:23:50 +0800

    20220418mac 未完成

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
-
+using namespace std;
+class IntArray{
+public:
+    int *data;
+    IntArray() = default;
+    IntArray(int n){
+        data = new int[n];
+    }
+    ~IntArray(){
+        delete[] data;
+    }
+};
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    IntArray obj(2);//This calls the constructor
+    obj.data[0] = -3;
+    obj.data[1] = 1;
+    IntArray obj2(obj);
+    cout << obj.data[0] << endl; // The result is -3
+    cout << obj2.data[1] << endl; // The result is ??
     return 0;
 }
